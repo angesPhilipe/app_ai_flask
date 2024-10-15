c5ee4f22a354e88341dc5e3c894b9a86b22a1a66
anges_p <angesphilou@gmail.com>
2024-10-15 17:42:53 +0200

    templates et index

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return"<marquee><h1>Hello Zizi content !!!!</h1></marquee>"
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
